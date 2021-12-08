10036b6f862028ba5d1b3cadf21f9ef2ebd930d1
BenPrevel <403rutabaga@gmail.com>
2021-12-08 20:32:54 +0100

    Quest Interface 08 done

---
+++ Fly.java
@@ -0,0 +1,17 @@
+
+public interface Fly {
+	
+	public void takeOff();
+	
+	public int ascend(int meters);
+	
+	public int descend (int meters);
+	
+	public void land();
+	
+	default void glide() {
+		System.out.println("Polymorphism!");
+	}
+	
+	}
+
